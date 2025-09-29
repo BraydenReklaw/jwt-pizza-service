8053d9e76f853b5f65d5ebacfeedf043eb84e230
BraydenW <braydenthewalker@gmail.com>
2025-09-29 12:03:13 -0600

    updated service.test.js for lint specification

--- src/service.test.js
+++ src/service.test.js
@@ -11,10 +11,12 @@ beforeAll(async () => {
 });
 
 test('login', async () => {
+  expect(testUserAuthToken).toBeDefined();
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
   expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 
   const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
   expect(loginRes.body.user).toMatchObject(user);
+  expect(password).toBe('a');
 });

d2af77e26dbe02890df3107df4bbba5bcd829747
BraydenW <braydenthewalker@gmail.com>
2025-09-30 12:00:35 -0600

    created tests up though addUser

--- src/database/database.test.js
+++ src/database/database.test.js
@@ -6,11 +6,40 @@ let origGetConnection;
 let origQuery;
 
 beforeAll(async () => {
-    origGetConnection = DB.pool.getConnection;
-    origQuery = DB.pool.query;
+    origGetConnection = DB.getConnection;
+    origQuery = DB.query;
 })
 
 afterAll(async () => {
-    DB.pool.getConnection = origGetConnection;
-    DB.pool.query = origQuery;
+    DB.getConnection = origGetConnection;
+    DB.query = origQuery;
+})
+
+test("get menu" , async () => {
+    const fakeMenu = [{ id: 1, title: 'Student', description: 'No topping, no sauce, just carbs', image: 'pizza9.png', price: 0.0001 }];
+    DB.getConnection = jest.fn().mockResolvedValue({ end: jest.fn() });
+    DB.query = jest.fn().mockResolvedValue(fakeMenu);
+    const res = await DB.getMenu();
+    expect(DB.getConnection).toHaveBeenCalled();
+    expect(DB.query).toHaveBeenCalledWith(expect.anything(), 'SELECT * FROM menu');
+    expect(res).toEqual(fakeMenu);
+})
+
+test("add menu item", async () => {
+    const newMenuItem = { title: 'Student', description: 'No topping, no sauce, just carbs', image: 'pizza9.png', price: 0.0001 };
+    DB.getConnection = jest.fn().mockResolvedValue({ end: jest.fn() });
+    DB.query = jest.fn().mockResolvedValue({ insertId: 9999 });
+    const res = await DB.addMenuItem(newMenuItem);
+    expect(DB.getConnection).toHaveBeenCalled();
+    expect(DB.query).toHaveBeenCalledWith(expect.anything(), 'INSERT INTO menu (title, description, image, price) VALUES (?, ?, ?, ?)', [newMenuItem.title, newMenuItem.description, newMenuItem.image, newMenuItem.price]);
+    expect(res).toEqual({...newMenuItem, id: 9999 });
+})
+
+test("add user", async () => {
+    const newUser = { name: 'testUser', email: 'test@test.com', password: 'testpassword', roles: [{ role: 'diner' }] };
+    DB.getConnection = jest.fn().mockResolvedValue({ end: jest.fn() });
+    DB.query = jest.fn().mockResolvedValue({ insertId: 1234 });
+    const res = await DB.addUser(newUser);
+    expect(DB.getConnection).toHaveBeenCalled();
+    expect(DB.query).toHaveBeenCalledTimes(2);
 })

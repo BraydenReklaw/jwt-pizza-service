7dffa951259bd191f4bad630230d5201755f1ca2
BraydenW <braydenthewalker@gmail.com>
2025-11-14 16:49:35 -0700

    logger.js assumedly complete

--- src/logger.js
+++ src/logger.js
@@ -33,14 +33,36 @@ class Logger {
         this.log('info', 'database', logData)
     }
 
+    factoryRequest(url, requestBody, responseBody, status) {
+        const logData = {
+            service: 'pizza-factory',
+            url,
+            requestBody,
+            responseBody,
+            status
+        }
+
+        const level = this.statusToLogLevel(status);
+        this.log(level, 'factory', logData)
+    }
+
+    exception(error) {
+        const logData = {
+            message: error.message,
+            stack: error.stack
+        };
+
+        this.log('error', 'exception', logData)
+    }
+
     log(level, type, logData) {
         const labels = {
             component: config.source,
             level,
             type
         };
-        const sanitized = this.sanitized(logData);
-        const values = [this.nowString(), sanitized];
+        const sanitizedData = this.sanitize(logData);
+        const values = [this.nowString(), sanitizedData];
 
         const logEvent = {
             streams: [{
@@ -60,5 +82,40 @@ class Logger {
 
     nowString() {
         return (Math.floor(Date.now()) *  1000000).toString()
+    }
+
+    sanitize(logData) {
+        let str = JSON.stringify(logData);
+
+        const redactions = [
+            /"password"\s*:\s*"[^"]*"/gi,
+            /"token"\s*:\s*"[^"]*"/gi,
+            /"authorization"\s*:\s*"[^"]*"/gi,
+            /"auth"\s*:\s*"[^"]*"/gi,
+            /"email"\s*:\s*"[^"]*"/gi  
+        ];
+
+        redactions.forEach(pattern => {
+            str = str.replace(pattern, (match) => {
+                const key = match.split(':')[0];
+                return `${key}: "${this.sanitized}`
+            })
+        })
+
+        return str;
+    }
+
+    sendLogToGrafana(event) {
+        const body = JSON.stringify(event);
+        fetch(`${config.url}`, {
+            method: 'post',
+            body: body,
+            headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${config.userId}:${config.apiKey}`,
+            },
+        }).then((res) => {
+            if (!res.ok) console.log('Failed to send log to Grafana');
+        });
     }
 }

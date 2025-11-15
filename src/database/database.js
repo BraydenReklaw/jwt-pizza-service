1b6deb21bcd633dbbfb8eb40a477f3fd9fdb9ce0
BraydenW <braydenthewalker@gmail.com>
2025-11-14 17:12:18 -0700

    database wired into logger

--- src/database/database.js
+++ src/database/database.js
@@ -4,6 +4,8 @@ const config = require('../config.js');
 const { StatusCodeError } = require('../endpointHelper.js');
 const { Role } = require('../model/model.js');
 const dbModel = require('./dbModel.js');
+const logger = require('../../logger.js')
+
 class DB {
   constructor() {
     this.initialized = this.initializeDatabase();
@@ -324,6 +326,7 @@ class DB {
 
   async query(connection, sql, params) {
     const [results] = await connection.execute(sql, params);
+    logger.db(sql, params)
     return results;
   }
 
